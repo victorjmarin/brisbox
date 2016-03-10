1917c76e05c1183ada64e21c30500cf5738535de
cripolgon <cripolgon@gmail.com>
2016-03-10 15:03:10 +0100

    Updated order_form

--- client/js/order-form.js
+++ client/js/order-form.js
@@ -1,9 +1,9 @@
-Template.Order-form.events({
+Template.orderForm.events({
 	'click .form-more-info': function(event){
 		var info = $(event.target);
 		var more_info = info.parent().parent().next();
 		if (more_info.css("display") == "none"){
-			more_info.slideDown("fast");
+			more_info.sldeDown("fast");
 		}else if(more_info.css("display") !== "none"){
 			more_info.slideUp("fast");
 		}
