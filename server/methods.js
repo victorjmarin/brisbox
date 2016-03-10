dd2c4dec4831a1c6c75b029cc43baa47e11d6bb1
cripolgon <cripolgon@gmail.com>
2016-03-10 12:59:14 +0100

    In progress. Added in i18n.json

--- server/methods.js
+++ server/methods.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 Meteor.methods({
     'chargeCard': function(stripeToken, amountForm) {
         var stripeKey = Meteor.settings.private.stripe.testSecretKey;
@@ -13,28 +12,3 @@ Meteor.methods({
         });
     }
 });
-=======
-Accounts.config({sendVerificationEmail: true, forbidClientAccountCreation: false, loginExpirationInDays: 10});
-
-
-
-function sendEmailVerification(userID, username, email){
-    if(email.contains("@alum.us.es") || email.contains("@alu.upo.es")) {
-        var validator = userID*17+userID*37;
-        var validatorLink="http://estuforce.meteor.com/verifyAccount?validator="+validator+"&username="+username;
-        try {
-            if (Meteor.isServer) { //Falta comprobar el idioma de la web para que el correo sea del mismo idioma.
-                Email.send({
-                    from: "estuforce@delabahia.es",
-                    to: email,
-                    subject: "Valida ahora tu cuenta de BRISBOX!",
-                    text: "Hola, "+username+". Bienvenid@ a BRISBOX. \nHaz click en el siguiente enlace para validar tu cuenta! "+validatorLink
-                });
-                MongoID.insert(username,userID, validator);
-            }
-        } catch (e) {
-            throw new Meteor.Error('Exception sending email.', e);
-        }
-    }
-}
->>>>>>> email_verification
