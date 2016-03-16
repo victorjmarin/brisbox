98733ab9baccdd3a7ae53a88ee3c2f18e6ea430d
blezerd <alexbalmar@gmail.com>
2016-03-16 23:34:01 +0100

    Send email verification
    The email is sended on the language of the web(Internacionalizated)

--- client/js/inscription-form.js
+++
@@ -1,8 +0,0 @@
-Template.inscriptionForm.events({
-    'submit': function (event) {
-        var schoolEmail = document.getElementById("emailSchool").value;
-        var name = document.getElementById("name").value;
-        var password = document.getElementById("password").value;
-        Accounts.createUser({username: name, email: schoolEmail, password:password, profile:"brisboxer", validate:false});
-    }
-});
