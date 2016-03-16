f1b0de230f18d54e3f27dc309d5f2ca2b3418906
AlbertFX91 <alberto.rojas.fndez@gmail.com>
2016-03-16 20:29:02 +0100

    added two examples users

--- server/bootstrap.js
+++ server/bootstrap.js
@@ -1,3 +1,18 @@
 Meteor.startup(function () {
     // code to run on server at startup
+    if (Meteor.users.find().count() === 0) {
+        var id_admin = Accounts.createUser({
+            username: 'admin',
+            email: 'admin@gmail.com',
+            password: 'croqueta1',
+        });
+        Roles.addUsersToRoles(id_admin, ['admin']);
+
+        var id_brisboxer1 = Accounts.createUser({
+            username: 'brisboxer1',
+            email: 'brisboxer1@alum.us.es',
+            password: 'brisboxer1',
+        });
+        Roles.addUsersToRoles(id_brisboxer1, ['brisboxer']);
+    }
 });
