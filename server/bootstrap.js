b836835b62fce9b802690793bc601513793b50b9
AlbertFX91 <alberto.rojas.fndez@gmail.com>
2016-03-21 23:16:00 +0100

    Added filters to routes, added atribute accepted to brisboxers and first template for accept brisboxers

--- server/bootstrap.js
+++ server/bootstrap.js
@@ -14,5 +14,46 @@ Meteor.startup(function () {
             password: 'brisboxer1',
         });
         Roles.addUsersToRoles(id_brisboxer1, ['brisboxer']);
+        Meteor.users.update(id_brisboxer1, {
+            $set: {
+                accepted: true
+            }
+        });
+
+        var id_brisboxer2 = Accounts.createUser({
+            username: 'brisboxer2',
+            email: 'brisboxer2@alum.us.es',
+            password: 'brisboxer2',
+        });
+        Roles.addUsersToRoles(id_brisboxer2, ['brisboxer']);
+        Meteor.users.update(id_brisboxer2, {
+            $set: {
+                accepted: false
+            }
+        });
+
+        var id_brisboxer3 = Accounts.createUser({
+            username: 'brisboxer3',
+            email: 'brisboxer3@alum.us.es',
+            password: 'brisboxer3',
+        });
+        Roles.addUsersToRoles(id_brisboxer3, ['brisboxer']);
+        Meteor.users.update(id_brisboxer3, {
+            $set: {
+                accepted: false
+            }
+        });
+
+        var id_brisboxer4 = Accounts.createUser({
+            username: 'brisboxer4',
+            email: 'brisboxer4@alum.us.es',
+            password: 'brisboxer4',
+        });
+        Roles.addUsersToRoles(id_brisboxer4, ['brisboxer']);
+        Meteor.users.update(id_brisboxer4, {
+            $set: {
+                accepted: false
+            }
+        });
     }
 });
