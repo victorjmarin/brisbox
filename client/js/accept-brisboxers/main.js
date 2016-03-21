ef87f151864dc573a34ab4975018b7651f07368a
AlbertFX91 <alberto.rojas.fndez@gmail.com>
2016-03-22 00:49:34 +0100

    Fixed published data

--- client/js/accept-brisboxers/main.js
+++ client/js/accept-brisboxers/main.js
@@ -2,6 +2,9 @@ Meteor.subscribe("brisboxers");
 
 Template.acceptBrisboxers.helpers({
     brisboxers: function(){
-    	return Meteor.users.find();
+    	//Quitamos el primer elemento de la coleccion ya que cuando estamos logueados, 
+    	//el primer elemento siempre es el usuario logueado, independiente de los objetos publicados.
+    	var brisboxers = Meteor.users.find().fetch();
+    	return brisboxers.slice(1, brisboxers.length);
     }
 });
