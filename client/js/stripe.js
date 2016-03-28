428e1b64bd8044695941cd93b9b126071d2a40d6
antmarpen <peligrossof@gmail.com>
2016-03-28 20:46:40 +0200

    Added error control for stripe form

--- client/js/stripe.js
+++ client/js/stripe.js
@@ -5,9 +5,13 @@ Template.stripe_form.onRendered(function () {
     var stripeKey = Meteor.settings.public.stripe.testPublishableKey;
     Stripe.setPublishableKey(stripeKey);
 
+    Session.set("stripe_error", null);
+});
 
-
-
+Template.stripe_form.helpers({
+    stripe_error: function() {
+        return Session.get("stripe_error");
+    }
 });
 
 Template.stripe_form.events({
@@ -20,14 +24,40 @@ Template.stripe_form.events({
         expYr = $('#exp-year').val();
         amountForm = $('#amount').val();
 
+        Session.set("stripe_error", null);
+
         Stripe.card.createToken({
             number: ccNum,
             cvc: cvc,
             exp_month: expMo,
             exp_year: expYr
         }, function(status, response) {
-            stripeToken = response.id;
-            Meteor.call('chargeCard', stripeToken, amountForm);
+
+            if(amountForm < 50){
+                var code = "invalid_amount";
+                Session.set("stripe_error", code);
+            }else if(status != 200){
+                var code = response.error.code;
+                Session.set("stripe_error", code);
+            }else {
+                stripeToken = response.id;
+                Meteor.call('chargeCard', stripeToken, amountForm, function(error, succeed){
+                    if(error){
+                        console.log(error);
+                    }else {
+                        var status = succeed.statusCode;
+                        if(status == null){
+                            status = succeed.status;
+                        }
+                            if(status != "succeeded"){
+                                var code = succeed.raw.code;
+                                Session.set("stripe_error", code);
+                            }else {
+                                alert("El pago se ha realizado correctamente.");
+                            }
+                    }
+                });
+            }
         });
     }
 });
--- server/methods.js
+++ server/methods.js
@@ -1,14 +1,24 @@
+var Future = Npm.require( 'fibers/future' );
+
 Meteor.methods({
     'chargeCard': function(stripeToken, amountForm) {
         var stripeKey = Meteor.settings.private.stripe.testSecretKey;
         var Stripe = StripeAPI(stripeKey);
 
+        var future = new Future();
+
         Stripe.charges.create({
             amount: amountForm,
             currency: 'eur',
             source: stripeToken
-        }, function(err, charge) {
-            console.log(err, charge);
+        },function(err, charge){
+            if(err){
+                future.return(err);
+            }else{
+                future.return(charge);
+            }
         });
+
+        return future.wait();
     }
 });
