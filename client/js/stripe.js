f9ad698a577ce7651cf80e10377579a38095992e
blezerd <alexbalmar@gmail.com>
2016-03-28 22:14:26 +0200

    Contact Us
    Created page contact us

--- client/js/stripe.js
+++ client/js/stripe.js
@@ -3,3 +3,30 @@ Template.stripe_form.onRendered(function() {
         accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
     });
 });
+
+Template.stripe_form.onRendered(function () {
+    var stripeKey = Meteor.settings.public.stripe.testPublishableKey;
+    Stripe.setPublishableKey(stripeKey);
+});
+
+Template.stripe_form.events({
+    'submit #payment-form': function(e) {
+        e.preventDefault();
+
+        ccNum = $('#number').val();
+        cvc = $('#cvc').val();
+        expMo = $('#exp-month').val();
+        expYr = $('#exp-year').val();
+        amountForm = $('#amount').val();
+
+        Stripe.card.createToken({
+            number: ccNum,
+            cvc: cvc,
+            exp_month: expMo,
+            exp_year: expYr
+        }, function(status, response) {
+            stripeToken = response.id;
+            Meteor.call('chargeCard', stripeToken, amountForm);
+        });
+    }
+});
