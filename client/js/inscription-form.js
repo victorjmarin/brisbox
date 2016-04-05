f5e05a10c57790c08c05e879a0353a29a6f43b50
blezerd <alexbalmar@gmail.com>
2016-04-06 01:15:23 +0200

    All CSS Fixed to film

--- client/js/inscription-form.js
+++ client/js/inscription-form.js
@@ -1,8 +1,25 @@
 AutoForm.addHooks(['inscriptionForm'], {
-    onSuccess: function(formType, result) {
+    onSuccess: function (formType, result) {
         Router.go("/welcome-view");
     },
-    onError: function(formType, result) {
+    onError: function (formType, result) {
         console.log("inscription-form error!")
     }
-})
+});
+
+Template.inscriptionForm.events({
+    'click #submit': function (event) {
+        var terms = document.getElementById('checkTerms').checked;
+        if(terms==false){
+            $('#errorTerms').css('display','block');
+            return false;
+        }else{
+            $('#errorTerms').css('display','none');
+            return true;
+        }
+    }
+});
+
+Template.inscriptionForm.onRendered(function (){
+    $('#errorTerms').css('display','none');
+});
