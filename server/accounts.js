18b994eaf1dcfeac5890d33f64fe4a62c78253da
blezerd <alexbalmar@gmail.com>
2016-04-19 19:38:43 +0200

    Merge remote-tracking branch 'remotes/origin/change-verified'

--- server/accounts.js
+++ server/accounts.js
@@ -5,14 +5,14 @@ Accounts.config({sendVerificationEmail: true, forbidClientAccountCreation: false
 Accounts.emailTemplates.siteName = "Brisbox";
 Accounts.emailTemplates.from = "Brisbox <hello@brisbox.com>";
 Accounts.emailTemplates.verifyEmail.subject = function (user) {
-    var currentLang = ServerSession.get("currentLang")
+    var currentLang = ServerSession.get("currentLang");
     return TAPi18n.__("email_validation_subject", null, currentLang);
 };
 Accounts.emailTemplates.verifyEmail.html = function (user, url) {
     var url = url.replace('#/', '');
-    var currentLang = ServerSession.get("currentLang")
-    Template.registerHelper('_', TAPi18n.__.bind(TAPi18n))
-    TAPi18next.setLng(currentLang)
+    var currentLang = ServerSession.get("currentLang");
+    Template.registerHelper('_', TAPi18n.__.bind(TAPi18n));
+    TAPi18next.setLng(currentLang);
     SSR.compileTemplate('verificationEmail', Assets.getText('verification-email.html'));
     return SSR.render("verificationEmail", {user: user, url: url, lng: currentLang});
 };
