17ba5b17386b0379fd2852aa0b30c4321231870b
Víctor J. Marín <victorjmarin@gmail.com>
2016-04-24 18:50:15 +0200

    Isotope implementation

--- client/js/list-all-orders/list-all-orders.js
+++ client/js/list-all-orders/list-all-orders.js
@@ -1,8 +1,65 @@
 Template.listAllOrders.helpers({
-    orders: function() {
+    orders: function () {
         var user_id = Meteor.userId();
-    	return Orders.find({
-		$where: "this.brisboxers.length < this.numberBrisboxers",
-		"brisboxers._id": {$ne: user_id}});
+        orders = Orders.find({
+            $where: "this.brisboxers.length < this.numberBrisboxers",
+            "brisboxers._id": {$ne: user_id}
+        });
+        return orders;
+    }
+});
+
+Template.listAllOrders.onRendered(function () {
+    var $container = $('.isotope').isotope({
+        // main isotope options
+        itemSelector: '.grid-item',
+        transitionDuration: '0.5s',
+        // set layoutMode
+        layoutMode: 'masonry',
+        // options for masonry layout mode
+        masonry: {
+            columnWidth: 328,
+            gutter: 30,
+        },
+        getSortData: {
+            hours: '[data-hours]',
+            left: '[data-left]'
+        }
+    });
+    var iso = $container.data('isotope');
+    $container.isotope('reveal', iso.items);
+})
+
+sortByHoursCrit = true;
+sortByLeftCrit = true;
+
+Template.listAllOrders.events({
+    'click #byDuration': function (event) {
+        event.preventDefault();
+        $('.isotope').isotope({
+            sortBy: 'hours',
+            sortAscending: sortByHoursCrit
+        });
+        sortByHoursCrit = !sortByHoursCrit
+    },
+    'click #byLeft': function (event) {
+        event.preventDefault();
+        $('.isotope').isotope({
+            sortBy: 'left',
+            sortAscending: sortByLeftCrit
+        });
+        sortByLeftCrit = !sortByLeftCrit
+    },
+    'click #random': function (event) {
+        event.preventDefault();
+        $('.isotope').isotope({
+            sortBy: 'random'
+        });
+    },
+    'click #original': function (event) {
+        event.preventDefault();
+        $('.isotope').isotope({
+            sortBy: 'original-order'
+        });
     }
 });
---
+++ client/js/list-all-orders/order-grid-item.js
@@ -0,0 +1,17 @@
+Template.orderGridItem.helpers({
+    left: function(){
+        return this.numberBrisboxers - this.brisboxers.length ;
+    },
+    dateFormat: function(){
+        return this.date.toLocaleString();
+    },
+    estimatedEarnings: function () {
+        return this.hours * 10;
+    }
+});
+
+Template.orderGridItem.events({
+    'click .join': function(event) {
+        Meteor.call("joinOrder", this);
+    }
+});
