eb355e4c55d6a6b4e4e39ae596ae01ffc996427d
Víctor J. Marín <victorjmarin@gmail.com>
2016-04-27 16:49:11 +0200

    Base layout for an order

--- client/js/list-all-orders/list-all-orders.js
+++ client/js/list-all-orders/list-all-orders.js
@@ -1,7 +1,6 @@
 Template.listAllOrders.helpers({
     orders: function () {
         var user_id = Meteor.userId();
-        console.log("HOURS_ASC")
         orders = Orders.find({
             $where: "this.brisboxers.length < this.numberBrisboxers",
             "brisboxers._id": {$ne: user_id}
--- client/js/list-all-orders/order-grid-item.js
+++ client/js/list-all-orders/order-grid-item.js
@@ -3,10 +3,23 @@ Template.orderGridItem.helpers({
         return this.numberBrisboxers - this.brisboxers.length;
     },
     dateFormat: function () {
-        return this.date.toLocaleString();
+        var locale = TAPi18n.getLanguage();
+        return moment(this.date).locale(locale).format('L');
+    },
+    timeFormat: function () {
+        return this.startMoment;
     },
     estimatedEarnings: function () {
         return this.hours * 10;
+    },
+    mapOptions: function () {
+        if (GoogleMaps.loaded()) {
+            return {
+                disableDefaultUI: true,
+                center: new google.maps.LatLng(37.3914105564361, -5.9591776906),
+                zoom: 12
+            };
+        }
     }
 });
 
@@ -14,4 +27,29 @@ Template.orderGridItem.events({
     'click .join': function (event) {
         Meteor.call("joinOrder", this);
     }
+});
+
+Template.orderGridItem.onRendered(function () {
+    GoogleMaps.load({
+        key: 'AIzaSyAfk4ikNH05OssyWavDvWImWFsf6oVXzzQ'
+    });
+});
+
+Template.orderGridItem.onCreated(function () {
+    GoogleMaps.ready('map', function (map) {
+        var geocoder = new google.maps.Geocoder();
+        var address = "Madrid";
+        geocoder.geocode({'address': address}, function (results, status) {
+            if (status == google.maps.GeocoderStatus.OK) {
+                var latitude = results[0].geometry.location.lat();
+                var longitude = results[0].geometry.location.lng();
+                var latLng = new google.maps.LatLng(latitude, longitude);
+                map.instance.setCenter(latLng);
+            }
+        });
+        var marker = new google.maps.Marker({
+            position: map.options.center,
+            map: map.instance
+        });
+    });
 });
