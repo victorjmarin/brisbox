9a46928079ba6453ea76fe11e5fbcbca58e81a4b
cripolgon <cripolgon@gmail.com>
2016-05-02 16:12:24 +0200

    Changes smtp

--- server/bootstrap.js
+++ server/bootstrap.js
@@ -1,8 +1,8 @@
 Meteor.startup(function () {
     // code to run on server at startup
     var smtp = {
-        username: 'brisboxteam@gmail.com',
-        password: 'cocretacorp69',
+        username: Meteor.settings.mail.user,
+        password: Meteor.settings.mail.password,
         server: 'smtp.gmail.com',
         port: 587
     };
