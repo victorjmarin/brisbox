fa7d75c30e4c9f47eeff4d5cc72258b6b0227538
AlbertFX91 <alberto.rojas.fndez@gmail.com>
2016-05-09 23:40:47 +0200

    Removed comments

--- client/js/order_dashboard/cancelcode_login.js
+++ client/js/order_dashboard/cancelcode_login.js
@@ -1,21 +1,4 @@
 Template.CancelCodeLogin.events({
-    /*'click #cancel': function (event) {
-        var cancelationCode = document.getElementById("cancelationCode").value;
-        var orderIdCodificado = sessionStorage.getItem("ord");
-        Meteor.call('cancelOrder', orderIdCodificado, cancelationCode, function (error, response) {
-            if (response) {
-                sessionStorage.removeItem("ord");
-                Router.go('OrderCanceledSuccess');
-            } else {
-                Router.go('OrderCancelFailed');
-            }
-        });
-    },
-    'click #dashboard': function (event) {
-        var orderIdCodificado = sessionStorage.getItem("ord");
-        Router.go('order_dashboard', {_id: orderIdCodificado});
-
-    },*/
     'submit #cancelCodeLoginForm': function (e) {
         e.preventDefault();
         var order_id = this._id;
